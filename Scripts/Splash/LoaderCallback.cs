76c9a00c88c293c509763d3809cafadb89b39962
harisaran <harisaranch@gmail.com>
2021-04-26 12:29:15 -0400

    first

---
+++ Scripts/Splash/LoaderCallback.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoaderCallback : MonoBehaviour
+{
+
+    private static int count = 0;
+
+    private void Update(){
+
+        count++;
+        
+        if (count == 40)
+        {
+            
+            Loader.LoaderCallback();
+        }
+    }
+
+}
